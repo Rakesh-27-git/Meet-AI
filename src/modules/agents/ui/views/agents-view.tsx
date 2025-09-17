59e4fff641ea6afda54aabc4ee5c47db6858b41a
Rakesh Choudhary <seervir987@gmail.com>
2025-09-17 13:14:24 +0530

    Add Meeting Data Table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,12 +4,12 @@ import { useRouter } from "next/navigation";
 import { useTRPC } from "@/trpc/client";
 
 import { columns } from "../components/columns";
-import { DataTable } from "../components/data-table";
 import { DataPagination } from "../components/data-pagination";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 
 import { ErrorState } from "@/components/error-state";
 import { EmptyState } from "@/components/empty-state";
+import { DataTable } from "@/components/data-table";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { LoadingState } from "@/components/loading-state";
 
